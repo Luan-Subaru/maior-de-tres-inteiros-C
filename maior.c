94bcb87ebe06b77176c5dd10ff4f8a05a3f33c1e
Luan <luan.patrique20@gmail.com>
2024-10-08 19:28:01 -0300

    Add files via upload

---
+++ maior.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+   int n1, n2, n3, maior;
+   
+   printf("\tMaior de três números!\n\n");
+   
+   printf("Digite um inteiro: ");
+   scanf("%d", &n1);
+   
+   printf("\nDigite outro inteiro: ");
+   scanf("%d", &n2);
+   
+   printf("\nDigite outro inteiro: ");
+   scanf("%d", &n3);
+   
+   if(n1 >= n2 && n1 >= n3){
+       printf("\n%d é o maior número!", n1);
+   }else {
+   
+    if(n2 >= n1 && n2 >= n3){
+       printf("\n%d é o maior número!", n2);
+   }
+       else{
+            if(n3 >= n2 && n3 >= n1){
+       printf("\n%d é o maior número!", n3);
+   }
+       }
+   }
+   
+   
+   return 0;
+}
